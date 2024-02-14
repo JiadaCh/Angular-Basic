3c366925f213fa699c9aff439c817a36facc22ae
JiadaCh <jiadachen9@gmail.com>
2024-02-14 08:39:21 +0100

    a

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import {RouterOutlet, RouterModule, RouterLink} from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet,RouterModule,RouterLink],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'Home ';
+}
---
+++ src/app/app.config.ts
@@ -0,0 +1,8 @@
+import { ApplicationConfig } from '@angular/core';
+import { provideRouter } from '@angular/router';
+
+import { routes } from './app.routes';
+import routeConfing from "./routes"
+export const appConfig: ApplicationConfig = {
+  providers: [provideRouter(routeConfing)]
+};
